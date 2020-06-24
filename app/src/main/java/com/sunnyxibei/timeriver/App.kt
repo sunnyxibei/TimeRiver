b40a9182136c421807af726fd969fed919ebadac
Yuanbin <jiayuanbin@gmail.com>
2020-06-24 16:38:31 +0800

    Update gradle plugin

--- app/src/main/java/com/sunnyxibei/timeriver/App.kt
+++ app/src/main/java/com/sunnyxibei/timeriver/App.kt
@@ -1,2 +1,26 @@
 package com.sunnyxibei.timeriver
 
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import timber.log.Timber
+
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        appContext = this
+        startKoin {
+            // Android context
+            androidContext(this@App)
+            // modules
+            modules(koinModule)
+        }
+        Timber.plant(Timber.DebugTree())
+    }
+
+    companion object {
+        lateinit var appContext: App
+    }
+}
---
+++ app/src/main/java/com/sunnyxibei/timeriver/KoinModule.kt
@@ -0,0 +1,8 @@
+package com.sunnyxibei.timeriver
+
+import org.koin.dsl.module
+
+val koinModule = module {
+
+
+}
