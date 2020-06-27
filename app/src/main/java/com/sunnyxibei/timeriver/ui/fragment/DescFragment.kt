6713ac9f4282b93a5ca570b1c7eb903a9bfb532d
Yuanbin <jiayuanbin@gmail.com>
2020-06-27 10:01:11 +0800

    Add custom font

--- app/src/main/java/com/sunnyxibei/timeriver/ui/fragment/DescFragment.kt
+++ app/src/main/java/com/sunnyxibei/timeriver/ui/fragment/DescFragment.kt
@@ -1,5 +1,6 @@
 package com.sunnyxibei.timeriver.ui.fragment
 
+import android.graphics.Typeface
 import android.os.Bundle
 import android.view.View
 import androidx.navigation.fragment.findNavController
@@ -11,10 +12,28 @@ import timber.log.Timber
 
 class DescFragment : BaseFragment() {
 
+    private val drawableIntArray = intArrayOf(
+        R.drawable.img_holder_1,
+        R.drawable.img_holder_2,
+        R.drawable.img_holder_3,
+        R.drawable.img_holder_4,
+        R.drawable.img_holder_5
+    )
+
+    private val stringArray: Array<String> by lazy {
+        requireContext().resources.getStringArray(R.array.poem_array)
+    }
+
     override fun getLayoutId(): Int = R.layout.fragment_desc
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        image_background.setImageResource(drawableIntArray.random())
+        tv_desc.run {
+            text = stringArray.random()
+            typeface = Typeface.createFromAsset(requireContext().assets, "font/YingZhangKaiShu.ttf")
+            animate().alpha(1f)
+        }
         draggable_frame.addListener(object : ElasticDragDismissFrameLayout.ElasticDragDismissCallback() {
             override fun onDragDismissed() {
                 Timber.d("DescFragment, onDragDismissed")
--- app/src/main/java/com/sunnyxibei/timeriver/ui/fragment/ListFragment.kt
+++ app/src/main/java/com/sunnyxibei/timeriver/ui/fragment/ListFragment.kt
@@ -1,15 +1,23 @@
 package com.sunnyxibei.timeriver.ui.fragment
 
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.format.DateUtils
+import android.text.style.TypefaceSpan
 import android.view.View
+import androidx.core.text.buildSpannedString
+import androidx.core.text.inSpans
+import androidx.core.text.scale
 import androidx.navigation.NavDirections
 import androidx.navigation.Navigator
 import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.findNavController
 import com.sunnyxibei.timeriver.R
 import com.sunnyxibei.timeriver.base.BaseFragment
-import com.sunnyxibei.timeriver.extension.setOnLimitClickListener
 import kotlinx.android.synthetic.main.fragment_list.*
+import java.util.*
 
 class ListFragment : BaseFragment() {
 
@@ -17,12 +25,33 @@ class ListFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.setOnLimitClickListener {
-            val extras: Navigator.Extras =
-                FragmentNavigatorExtras(image_destination to "image_destination")
+        view.setOnLongClickListener {
+            val extras: Navigator.Extras = FragmentNavigatorExtras(image_background to "image_background")
             val descFragmentDirections: NavDirections = DescFragmentDirections.actionGlobalDescFragment()
             findNavController().navigate(descFragmentDirections.actionId, null, null, extras)
+            true
         }
+        val kaiShu = Typeface.createFromAsset(requireContext().assets, "font/YingZhangKaiShu.ttf")
+        tv_title.text = buildSpannedString {
+            append("距离")
+            scale(2.0f) {
+                inSpans(TypefaceSpan("default")) {
+                    append("2022/6/30")
+                }
+            }
+            append("祥生云境交房")
+        }
+        tv_title.typeface = kaiShu
+        tv_day.typeface = kaiShu
+        tv_remain_day.text =
+            "${(Date("2022/06/30").time - (System.currentTimeMillis())) / DateUtils.DAY_IN_MILLIS}"
+        val scaleXHolder = PropertyValuesHolder.ofFloat(View.SCALE_X, 1.1f, 1.25f)
+        val scaleYHolder = PropertyValuesHolder.ofFloat(View.SCALE_Y, 1.1f, 1.25f)
+        ObjectAnimator.ofPropertyValuesHolder(image_background, scaleXHolder, scaleYHolder).apply {
+            duration = 10000L
+            repeatMode = ObjectAnimator.REVERSE
+            repeatCount = ObjectAnimator.INFINITE
+        }.start()
     }
 
 }
